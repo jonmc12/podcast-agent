a5a952aa9c32a687f6e88a757ae4f21d7fd8eda0
Jonathan McCoy <jonathan@collectiveframe.com>
2023-05-17 10:28:22 -0700

    WIP - upload pipeline

--- podcast_agent/services/transcriber.py
+++ podcast_agent/services/transcriber.py
@@ -1,4 +1,10 @@
+import asyncio
+
+import requests
+
 from podcast_agent.client.transcriber import TranscriberClient
+from podcast_agent.services.podcast_api import PodcastApiService
+from podcast_agent.services.supabase import SupabaseService
 
 
 class TranscriberService:
@@ -27,3 +33,41 @@ class TranscriberService:
         return self.client.read_from_url(
             url=url,
         )
+
+    def transcribe_and_upload(self):
+        """
+        Transcribe an mp3 from a ListenNotes URL and upload to Supabase
+        """
+        podcast_api_service = PodcastApiService()
+        loop = asyncio.get_event_loop()
+        # transcription = self.transcribe_from_url(url=url)
+        episode_response = loop.run_until_complete(podcast_api_service.get_episode(
+            id="87a935f22138437baa4b0a1043895e9b",
+            show_transcript=1
+        ))
+
+        audio_url = episode_response["audio"]
+
+        if episode_response["transcript"] is not None:
+            transcription = episode_response["transcript"] 
+        else:
+            audio_response = requests.get(audio_url)
+            if audio_response.status_code == 200:
+                with open("podcast.mp3", "wb") as file:
+                    for chunk in audio_response.iter_content(chunk_size=1024):
+                        file.write(chunk)
+                print("Download completed.")
+            else:
+                print("Failed to download file.")
+
+            supabase_service = SupabaseService()
+            supabase_service.upload_file(
+                bucket_name="episode-audio-files",
+                file_name="podcast.mp3",
+            )
+
+            transcription = self.transcribe_from_file(file_name="podcast.mp3")
+
+        return transcription
+
+
